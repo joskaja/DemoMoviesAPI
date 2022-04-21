ef25eb89b33012603ff823e36fd9505f3f538dfb
Jan Joska <joskaja1@uhk.cz>
2022-04-21 16:01:44 +0200

    CORS disabled

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
+const cors = require('cors');
+
 const connectDB = require('./config/db');
 const { handleError } = require('./middleware/errorMiddleware');
 
@@ -9,7 +11,9 @@ const { typeDefs, resolvers } = require('./graphql');
 
 
 const app = express();
+app.use(cors())
 connectDB();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
